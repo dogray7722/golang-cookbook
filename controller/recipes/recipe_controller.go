679397bd05d255d9c35d10e302c6f394cd83b557
Dave Gray <dave@choozle.com>
2020-09-13 14:23:36 -0600

    build out framework

---
+++ controller/recipes/recipe_controller.go
@@ -0,0 +1,23 @@
+package recipes
+
+import "github.com/gin-gonic/gin"
+
+func Create(c *gin.Context) {
+	//TODO Implement
+}
+
+func List(c *gin.Context) {
+	//TODO Implement
+}
+
+func Get(c *gin.Context) {
+	//TODO Implement
+}
+
+func Update(c *gin.Context) {
+	//TODO Implement
+}
+
+func Delete(c *gin.Context) {
+	//TODO Implement
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/dogray7722/golang-cookbook/app"
+)
+
+func main() {
+	app.StartApplication()
+}
