83c94b01fc4eded7b9dc99d9deb1f384b2ed59bf
Dave Gray <dave@choozle.com>
2020-09-14 21:30:06 -0600

    add error handling object

--- domain/recipes/recipe_dto.go
+++ domain/recipes/recipe_dto.go
@@ -1,9 +1,9 @@
 package recipes
 
 type Recipe struct {
-	Id int64 `json:"id"`
-	Ingredients []string `json:"ingredients"`
-	Instructions string `json:"instructions"`
-	DateCreated string `json:"date_created"`
-	Status string `json:"status"`
+	Id           int64    `json:"id"`
+	Ingredients  []string `json:"ingredients"`
+	Instructions string   `json:"instructions"`
+	DateCreated  string   `json:"date_created"`
+	Status       string   `json:"status"`
 }
