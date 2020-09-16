cc556c27c2a6dfe9d867c7692e32605d7f5e6011
Dave Gray <dave@choozle.com>
2020-09-16 07:01:56 -0600

    redefine domain

--- domain/recipes/recipe_dto.go
+++ domain/recipes/recipe_dto.go
@@ -1,9 +1,14 @@
 package recipes
 
 type Recipe struct {
-	Id           int64    `json:"id"`
-	Ingredients  []string `json:"ingredients"`
-	Instructions string   `json:"instructions"`
-	DateCreated  string   `json:"date_created"`
-	Status       string   `json:"status"`
+	Id           int64        `json:"id"`
+	Ingredients  []Ingredient `json:"ingredients"`
+	Instructions string       `json:"instructions"`
+	DateCreated  string       `json:"date_created"`
+	Status       string       `json:"status"`
+}
+
+type Ingredient struct {
+	servingSize string
+	name        string
 }
