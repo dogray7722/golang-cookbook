80c33029dee65b2fbcb08fe8a004b84923b95b67
David Gray <dogray77@gmail.com>
2020-10-07 22:02:07 -0600

    use the dao to return the list

--- service/recipes_service.go
+++ service/recipes_service.go
@@ -28,11 +28,13 @@ func GetRecipe(recipeId int64) (*recipes.Recipe, *errors.RestErr) {
 	return result, nil
 }
 
-func ListRecipes() ([]*recipes.Recipe, *errors.RestErr) {
-	rec := recipes.Recipe{}
-	if err := rec.List(); err != nil {
+func ListRecipes() ([]recipes.Recipe, *errors.RestErr) {
+	dao := &recipes.Recipe{}
+	recipes, err := dao.List()
+	if err != nil {
 		return nil, err
 	}
+	return recipes, nil
 }
 
 func UpdateRecipe(recipe recipes.Recipe) (*recipes.Recipe, *errors.RestErr) {
