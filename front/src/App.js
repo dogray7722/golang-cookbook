53b67f6e093b3aa734171ad0941e87c998296cf8
David Gray <dogray77@gmail.com>
2021-06-10 09:24:12 -0600

    recipe component build out

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
+import Recipes from './components/recipes'
 
 function App() {
   return (
     <Container maxWidth="sm">
       <h1>Go Lang Cookbook</h1>
+      <Recipes />
     </Container>
   );
 }
--- front/src/components/recipes.js
+++ front/src/components/recipes.js
@@ -1,11 +1,10 @@
-import { render } from '@testing-library/react'
-import React from 'react'
+import React, { useEffect, useState }from 'react'
 
 export const recipes = () => {
   
   return (
-    <div>
+    <>
       
-    </div>
+    </>
   )
 }
