eb814e31b745aac141ff95aae3fb8bfcbf2a08ff
David Gray <dogray77@gmail.com>
2021-06-19 19:01:03 -0600

    update fonts for non material ui components

--- front/src/components/recipes.js
+++ front/src/components/recipes.js
@@ -9,6 +9,9 @@ const useStyles = makeStyles({
   },
   avatar: {
     backgroundColor: purple[500]
+  },
+  content: {
+    padding: "8px"
   }
 })
 
@@ -45,14 +48,22 @@ const content = recipes.map(recipe => {
         </Avatar>
       }
     />
-    <CardContent>
-      <ul>
+    <CardContent className={classes.content}>
+      <Typography variant="subtitle1" color="textPrimary">
+        Ingredients
+      </Typography>
+      <Typography color="textSecondary">
+      <ul style={{listStyle: "none", margin: "3px", fontSize: "14px", fontFamily: "Roboto, Helvetica, Arial, sans-serif"}}>
         {recipe.ingredients.map(ingredient => (
             <li key={ingredient.id}>
               {`${ingredient.serving_size} ${ingredient.item}`}
             </li>
         ))}
       </ul>
+      </Typography>
+      <Typography variant="subtitle1" color="textPrimary">
+        Instructions
+      </Typography>
       <Typography variant="body2" color="textSecondary" component="p">
         {recipe.instructions}
       </Typography>
