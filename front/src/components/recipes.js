b73c4216ad0750c74ede0ff0155fd3c481c259e0
David Gray <dogray77@gmail.com>
2021-06-19 18:45:33 -0600

    more styling

--- front/src/components/recipes.js
+++ front/src/components/recipes.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState }from 'react'
-import {Card, CardHeader, makeStyles, Avatar} from '@material-ui/core'
+import {Card, CardHeader, makeStyles, Avatar, CardContent, Typography} from '@material-ui/core'
 import purple from "@material-ui/core/colors/purple";
 
 const useStyles = makeStyles({
@@ -35,23 +35,28 @@ const content = recipes.map(recipe => {
       className={classes.recipeCard}
   >
     <CardHeader
+      titleTypographyProps={{variant:'h6' }}
       title={recipe.name}
       avatar={
         <Avatar
           aria-label="recipe" className={classes.avatar}
         >
-          R
+          {recipe.name.charAt(0).toUpperCase()}
         </Avatar>
       }
     />
-    <ul>
-      {recipe.ingredients.map(ingredient => (
-      <li key={ingredient.id}>
-        {`${ingredient.serving_size} ${ingredient.item}`}      
-      </li>
-      ))} 
-    </ul>
-    <p>{recipe.instructions}</p>
+    <CardContent>
+      <ul>
+        {recipe.ingredients.map(ingredient => (
+            <li key={ingredient.id}>
+              {`${ingredient.serving_size} ${ingredient.item}`}
+            </li>
+        ))}
+      </ul>
+      <Typography variant="body2" color="textSecondary" component="p">
+        {recipe.instructions}
+      </Typography>
+    </CardContent>
   </Card>
   );
 })
