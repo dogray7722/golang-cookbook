e3788aae397b43e389d0f568c969e6397289463d
David Gray <dogray77@gmail.com>
2021-09-06 21:01:50 -0600

    elevation scroll effect for the app bar

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -18,6 +18,14 @@ function App() {
         >
             Favorite Recipes
         </Typography>
+        {[...new Array(120)]
+            .map(
+                () => `Cras mattis consectetur purus sit amet fermentum.
+                Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+                Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+            )
+            .join('\n')}
         {/*<RecipeForm />*/}
         {/*<Recipes />*/}
     </Container>
