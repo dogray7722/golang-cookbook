bb98f42127b4f46f1084a591493cb569cfaa0fe5
David Gray <dogray77@gmail.com>
2021-09-06 21:20:19 -0600

    add in theme provider

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
+import { ThemeProvider } from "@material-ui/styles";
+import theme from './ui/theme';
 import { Container, Typography } from '@material-ui/core';
-import Recipes from './recipes'
-import RecipeForm from "./recipeForm";
+import Recipes from './ui/recipes'
+import RecipeForm from "./ui/recipeForm";
 import Header from '../components/ui/header'
 
 
 function App() {
 
   return (
-    <>
-    <Container maxWidth="sm">
-        <Header/>
-        <Typography
-            color="secondary"
-            variant="h2"
-            align="center"
-        >
-            Favorite Recipes
-        </Typography>
-        {/*<RecipeForm />*/}
-        {/*<Recipes />*/}
-    </Container>
-    </>
+    <ThemeProvider theme={theme}>
+        <Container maxWidth="sm">
+            <Header/>
+            <Typography
+                color="secondary"
+                variant="h2"
+                align="center"
+            >
+                Favorite Recipes
+            </Typography>
+            {/*<RecipeForm />*/}
+            {/*<Recipes />*/}
+        </Container>
+    </ThemeProvider>
   );
 }
 
