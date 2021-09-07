fd698711dbf286ea8e20ede93f909f36d554a5a0
David Gray <dogray77@gmail.com>
2021-09-06 21:04:57 -0600

    clean up elevation scroll

--- front/src/components/ui/header.js
+++ front/src/components/ui/header.js
@@ -4,14 +4,11 @@ import Toolbar from '@material-ui/core/Toolbar'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 
 function ElevationScroll(props) {
-    const { children, window } = props;
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
+    const { children } = props;
+
     const trigger = useScrollTrigger({
         disableHysteresis: true,
-        threshold: 0,
-        target: window ? window() : undefined,
+        threshold: 0
     });
 
     return React.cloneElement(children, {
