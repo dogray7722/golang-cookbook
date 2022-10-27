107f00953a217f718bfc847b7b6334f82d81e7b2
David Gray <dogray77@gmail.com>
2022-10-27 12:45:48 -0600

    fix list aendpoint

--- app/recipe.go
+++ app/recipe.go
@@ -73,14 +73,24 @@ func (server *Server) getRecipe(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, recipe)
 }
 
+type listRecipesRequest struct {
+	PageID int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
 func (server *Server) listRecipes(ctx *gin.Context) {
-	var params db.ListRecipesParams
-	if err := ctx.ShouldBindJSON(&params); err != nil {
+	var req listRecipesRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
+
+	arg := db.ListRecipesParams{
+		Limit: req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
 	
-	recipes, err := server.store.ListRecipes(ctx, params) 
+	recipes, err := server.store.ListRecipes(ctx, arg) 
 		if err != nil {
 			ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 			return
